85c7d8e206f1449d965a2d37927a78d33253acb3
weblong <1173402105@qq.com>
2024-07-21 07:17:23 +0800

    请求地址修改

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,13 +1,13 @@
 let baseURL = "";
 if (process.env.VUE_APP_BASE_NAME === "test") {
-  baseURL = "https://api.lietkid.com.cn";
+  baseURL = "https://api.elietkid.com.cn";
 } else if (process.env.VUE_APP_BASE_NAME === "production") {
-  baseURL = "https://api.lietkid.com.cn";
+  baseURL = "https://api.elietkid.com.cn";
 } else {
   console.log(process.env.VUE_APP_BASE_NAME);
   // 开发环境
   // baseURL = "http://120.55.181.91:8096";
-  baseURL = "https://api.lietkid.com.cn";
+  baseURL = "https://api.elietkid.com.cn";
   // baseURL = "https://api.elietkid.com.cn";
 }
 
